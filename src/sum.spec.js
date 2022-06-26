9cf2b34630385bf2072672300461e4d70b811ef3
hidekazoo <tanakahidek@gmail.com>
2022-06-26 18:11:30 +0900

    Tests to see if the test will not be published in the event of a test failure.

--- src/sum.spec.js
+++ src/sum.spec.js
@@ -2,7 +2,11 @@ import { describe, test, assert } from "vitest";
 import { sum } from "./sum.mjs";
 
 describe("sum", () => {
-  test("1+2", () => {
+  test("1+2 = 3", () => {
     assert.equal(sum(1, 2), 3);
+  });
+
+  test("failed test 1+3", () => {
+    assert.equal(sum(1, 3), 3);
   });
 });
